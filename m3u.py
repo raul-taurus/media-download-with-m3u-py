1a2884d3e0ea4039344c85b563b23f498a5a9c32
Raul Li <raul.guoping@gmail.com>
2021-12-23 07:00:03 +0000

    Remove headers

--- m3u.py
+++ m3u.py
@@ -1,40 +1,27 @@
+"""System module."""
 import os
-import subprocess
 import sys
 import http.client
 from urllib.parse import urlparse
 
 
-def main(url, target):
+def main(m3u8_url, target):
+    """The main function to download media files."""
+    uri = urlparse(m3u8_url)
 
-    u = urlparse(url)
+    conn = http.client.HTTPSConnection(uri.hostname, uri.port)
 
-    conn = http.client.HTTPSConnection(u.hostname, u.port)
+    last_slash = uri.path.rfind("/")
+    relative_url = f"{uri.scheme}://{uri.netloc}{uri.path[:last_slash]}/"
 
-    headers = {
-        'accept': '*/*',
-        'accept-encoding': 'gzip, deflate, br',
-        'accept-language': 'en-US,en;q=0.9',
-        'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="96", "Google Chrome";v="96"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Linux"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'same-site',
-        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36',
-    }
-
-    last_slash = u.path.rfind("/")
-    relative_url = f"{u.scheme}://{u.netloc}{u.path[:last_slash]}/"
-
-    conn.request("GET", url, headers=headers)
+    conn.request("GET", m3u8_url)
     response = conn.getresponse()
 
     if response.status != 200:
         print(response.status)
         return
 
-    m3u = response.read().decode('utf-8')
+    m3u = response.read().decode("utf-8")
     m3u_lines = m3u.splitlines()
     media = open(target, "wb")
 
@@ -44,12 +31,12 @@ def main(url, target):
 
     for line in part_urls:
         part_url = relative_url + line
-        conn.request("GET", part_url, headers=headers)
+        conn.request("GET", part_url)
         part = conn.getresponse()
         media.write(part.read())
 
         part_index += 1
-        percent = "{:9.2f}".format(float(part_index)*100/part_total)
+        percent = f"{float(part_index) * 100 / part_total:9.2f}"
         sys.stdout.write(f"{percent}%, ({part_index}/{part_total})\r")
         sys.stdout.flush()
 
@@ -59,8 +46,8 @@ def main(url, target):
 
 url = sys.argv[1]
 
-temp_output = "media.mp4"
-final_output = "final-media.mp4"
-main(url, temp_output)
+TEMP_OUTPUT = "media.mp4"
+FINAL_OUTPUT = "final-media.mp4"
+main(url, TEMP_OUTPUT)
 
-os.system(f"ffmpeg -i {temp_output} -c copy {final_output}")
+os.system(f"ffmpeg -i {TEMP_OUTPUT} -c copy {FINAL_OUTPUT}")
